f00178a1af73166a13eb36f5b377c7c4f39d1235
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-05-11 10:55:16 +0300

    Создание приложения крипто-кошелек

--- flutter/crypto_coins_list/lib/main.dart
+++ flutter/crypto_coins_list/lib/main.dart
@@ -13,16 +13,15 @@ class CriptoCurrencyListApp extends StatelessWidget {
     return MaterialApp(
       title: 'Cripto Currency List',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blueGrey,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 39, 38, 38),
+        primarySwatch: Colors.yellow,
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w400,
+            fontSize: 22,
+          ),
+        ),
       ),
       home: const MyHomePage(title: 'Cripto Currency List'),
     );
@@ -70,34 +69,13 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, i) => ListTile(
+          title: Text(
+            "Coins",
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
