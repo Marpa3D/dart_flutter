5470daa21d282135cb3c16e3d21d825eb6604503
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2023-05-17 19:58:25 +0300

    Виджет Text.rich()

--- flutter/awesome_app/lib/main.dart
+++ flutter/awesome_app/lib/main.dart
@@ -2,14 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Awesome app',
       home: Scaffold(
-        body: Center(
-          child: Text(
-            'Привет. Заряжено во Flutter!',
-            textDirection: TextDirection.ltr,
+        appBar: AppBar(
+          title: Text(
+            'Dashboard'.toUpperCase(),
+          ),
+        ),
+        body: const Center(
+          child: Text.rich(
+            TextSpan(
+              text: 'My',
+              children: [
+                TextSpan(
+                  text: 'Flutter',
+                  style: TextStyle(
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+                TextSpan(
+                  text: 'App',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
